396540d8a363ddf7d5c825a1ad515dcebeddcf1c
haiyu <haiyu@good.com>
2022-07-30 14:36:01 +0800

    加入redis，缓存验证码和菜品

---
+++ src/main/java/com/itheima/reggie/controller/OrderDetailController.java
@@ -0,0 +1,32 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Orders;
+import com.itheima.reggie.service.OrderDetailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 订单明细
+ */
+@Slf4j
+@RestController
+@RequestMapping("/orderDetail")
+public class OrderDetailController {
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders){
+        return null;
+    }
+
+
+}
